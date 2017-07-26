a6056a5da829d8dfb60b4032cb8564fbb92945ae
Brainya <bombird.dtb@gmail.com>
2017-07-26 22:57:26 +0900

    Add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(author: '名無しさん', body: 'これは本文です')
+Post.create!(author: '豊田サリー', body: 'テクマクマヤコンテクマクマヤコン')
+Post.create!(author: 'ガンジー', body: "非暴力\n非服従\n言葉は要らない")
